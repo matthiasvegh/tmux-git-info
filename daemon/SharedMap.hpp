46aee87e723c36c50c3a24ea4e357f9432aaed66
Matthias Vegh <matyas.vegh@gmail.com>
2014-11-26 20:39:43 +0100

    Use managed_shared_memory and construct map for SharedMap

--- daemon/SharedMap.hpp
+++ daemon/SharedMap.hpp
@@ -1,15 +1,22 @@
 #ifndef SHARED_MAP_HPP_
 #define SHARED_MAP_HPP_
 
-#include <boost/interprocess/shared_memory_object.hpp>
-#include <boost/interprocess/mapped_region.hpp>
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/containers/map.hpp>
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/containers/map.hpp>
 
 template<typename Key, typename Value>
 class SharedMap {
 
-	boost::interprocess::shared_memory_object sharedMemory;
-	boost::interprocess::mapped_region sharedRegion;
+	boost::interprocess::managed_shared_memory sharedMemory;
 	constexpr static const char* sharedMemoryName = "tmuxinfod_shared_memory";
+	constexpr static const char* mapImplInstanceName = "SharedMapImpleInstance";
+	using Allocator = boost::interprocess::allocator<std::pair<const Key, Value>, boost::interprocess::managed_shared_memory::segment_manager>;
+	using SharedMapImpl = boost::interprocess::map<Key, Value, std::less<Key>, Allocator>;
+
+	const Allocator allocatorInstance;
+	SharedMapImpl* sharedMapImplInstance;
 
 public:
 
@@ -17,13 +24,10 @@ public:
 		sharedMemory(
 			boost::interprocess::open_or_create,
 			sharedMemoryName,
-			boost::interprocess::read_write),
-		sharedRegion(
-			sharedMemory,
-			boost::interprocess::read_write)
+			65536),
+		allocatorInstance(sharedMemory.get_segment_manager()),
+		sharedMapImplInstance(sharedMemory.find_or_construct(mapImplInstanceName))
 	{
-		sharedMemory.truncate(1024);
-		// maybe make dynamic?
 	}
 
 };
