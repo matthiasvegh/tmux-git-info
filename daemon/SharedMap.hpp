7c735f98cfebeda06a2a99ea7c2948b4ad2ad8c6
Matthias Vegh <matyas.vegh@gmail.com>
2014-11-26 21:03:26 +0100

    Add mutex and basic map functionality to SharedMap

--- daemon/SharedMap.hpp
+++ daemon/SharedMap.hpp
@@ -5,18 +5,24 @@
 #include <boost/interprocess/containers/map.hpp>
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/containers/map.hpp>
+#include <boost/interprocess/sync/interprocess_recursive_mutex.hpp>
+#include <boost/interprocess/sync/scoped_lock.hpp>
 
 template<typename Key, typename Value>
 class SharedMap {
 
 	boost::interprocess::managed_shared_memory sharedMemory;
 	constexpr static const char* sharedMemoryName = "tmuxinfod_shared_memory";
-	constexpr static const char* mapImplInstanceName = "SharedMapImpleInstance";
+	constexpr static const char* mapImplInstanceName = "tmuxinfod_SharedMapImpleInstance";
+	constexpr static const char* sharedMutexName = "tmuxinfod_SharedMutex";
 	using Allocator = boost::interprocess::allocator<std::pair<const Key, Value>, boost::interprocess::managed_shared_memory::segment_manager>;
 	using SharedMapImpl = boost::interprocess::map<Key, Value, std::less<Key>, Allocator>;
+	using SharedMutex = boost::interprocess::interprocess_mutex;
 
 	const Allocator allocatorInstance;
 	SharedMapImpl* sharedMapImplInstance;
+
+	SharedMutex* sharedMutex;
 
 public:
 
@@ -26,9 +32,16 @@ public:
 			sharedMemoryName,
 			65536),
 		allocatorInstance(sharedMemory.get_segment_manager()),
-		sharedMapImplInstance(sharedMemory.find_or_construct(mapImplInstanceName))
+		sharedMapImplInstance(sharedMemory.find_or_construct(mapImplInstanceName)),
+		sharedMutex(sharedMemory.find_or_construct(sharedMutexName))
 	{
 	}
+
+	Value& operator[](const Key& key) {
+		boost::interprocess::scoped_lock<SharedMutex> lock(*sharedMutex);
+		return (*sharedMapImplInstance)[key];
+	}
+
 
 };
 
