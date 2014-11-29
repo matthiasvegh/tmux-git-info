6eb8d039491f85c3e0b4aaf45392e6de16fcf5ce
Matthias Vegh <matyas.vegh@gmail.com>
2014-11-29 20:40:20 +0100

    Make sharedmemory removal unconditional

--- daemon/SharedMap.hpp
+++ daemon/SharedMap.hpp
@@ -14,7 +14,6 @@ class SharedMap {
 	constexpr static const char* sharedMemoryName = "tmuxinfod_shared_memory" __DATE__ __TIME__;
 	constexpr static const char* mapImplInstanceName = "tmuxinfod_SharedMapImpleInstance";
 	constexpr static const char* sharedMutexName = "tmuxinfod_SharedMutex";
-	constexpr static const char* referenceCountName = "tmuxinfod_referenceCount";
 
 	using Comparator = std::less<Key>;
 	using Allocator = boost::interprocess::allocator<std::pair<Key const, Value>, boost::interprocess::managed_shared_memory::segment_manager>;
@@ -25,7 +24,6 @@ class SharedMap {
 	Allocator allocatorInstance;
 	SharedMapImpl* sharedMapImplInstance;
 	SharedMutex* sharedMutex;
-	std::size_t* referenceCount;
 
 
 public:
@@ -41,12 +39,8 @@ public:
 				),
 		sharedMutex(
 				sharedMemory.find_or_construct<SharedMutex>(sharedMutexName)()
-				),
-		referenceCount(
-				sharedMemory.find_or_construct<std::size_t>(referenceCountName)(0))
-
+				)
 	{
-		++*referenceCount;
 	}
 
 	Value operator[](const Key& key) const {
@@ -65,12 +59,7 @@ public:
 	}
 
 	~SharedMap() {
-		boost::interprocess::scoped_lock<SharedMutex> lock(*sharedMutex);
-		if(*referenceCount > 0) {
-			if(--*referenceCount == 0) {
-				boost::interprocess::shared_memory_object::remove(sharedMemoryName);
-			}
-		}
+		boost::interprocess::shared_memory_object::remove(sharedMemoryName);
 	}
 
 
