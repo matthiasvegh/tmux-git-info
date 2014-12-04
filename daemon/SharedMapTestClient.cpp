c4037d97bb62bd94f8bf707203b38a37099dc4c7
Matthias Vegh <matyas.vegh@gmail.com>
2014-12-04 16:52:33 +0100

    Add SharedMapTestClient.cpp

---
+++ daemon/SharedMapTestClient.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include "SharedMap.hpp"
+
+int main(int argc, char** argv) {
+
+	if(argc != 2 || argc != 4) {
+		exit(1);
+	}
+
+	SharedMap<std::size_t, SharedString> sm;
+
+	if(argc == 2) {
+		std::cout<<sm[std::atoi(argv[1])]<<std::endl;
+	}
+
+	if(argc == 4) {
+		sm[std::atoi(argv[1])] = argv[2];
+		std::this_thread::sleep_for(std::chrono::seconds(std::atoi(argv[3])));
+	}
+
+}
